c460bb38b39e48b381b1dcb948f72c4cef484ef8
Federico <saipy06@gmail.com>
2020-09-19 20:23:52 -0400

    Se agrego la variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -19,8 +19,21 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 
 } else {
-    urlDB = 'mongodb+srv://ysapy:kqYjgqTez8iHRSqW@cluster0.ktbxh.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
+
+/*
+para ello se ejecutaron las siguiente lineas
+heroku config 				// para ver las variables de heroku
+
+heroku config:set  MONGO_URI="mongodb+srv://ysapy:kqYjgqTez8iHRSqW@cluster0.ktbxh.mongodb.net/cafe?retryWrites=true&w=majority"
+
+heroku config:get nombre				// trae el contenido de la variable
+
+
+
+*/
+
 
 //urlDB = 'mongodb+srv://ysapy:kqYjgqTez8iHRSqW@cluster0.ktbxh.mongodb.net/cafe?retryWrites=true&w=majority';
 process.env.URLDB = urlDB;
